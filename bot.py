94e58ce6918d91427b2e4fc22a4c38b68c303846
alkhyeli82 <druae@live.com>
2025-09-27 16:33:00 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,27 @@
+import os
 import telebot
 from flask import Flask, request
 
+# توكن البوت بين علامتي اقتباس
 TOKEN = "8477120330:AAGNqSX4Kb1wMhQcGqeNRyTZfqJhZw2Vbdg"
-bot = telebot.TeleBot(TOKEN)
 
-# عرّف Flask app
+bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-# Webhook endpoint
+# يستقبل التلغرام التحديثات عبر الويب هوك
 @app.route("/" + TOKEN, methods=["POST"])
 def getMessage():
     json_str = request.get_data().decode("UTF-8")
     update = telebot.types.Update.de_json(json_str)
     bot.process_new_updates([update])
-    return "!", 200
+    return "ok", 200
 
-# عشان تختبر البوت
+# صفحة فحص سريعة
 @app.route("/")
 def webhook():
     return "البوت شغال ✅", 200
 
-
-# هنا لازم تخلي البوت يربط نفسه بالرابط
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     bot.remove_webhook()
     bot.set_webhook(url="https://forex-bot-mwej.onrender.com/" + TOKEN)
